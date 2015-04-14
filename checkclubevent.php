e1bf58fe4ead4f4eaaf31f34e019ced189b79010
cw1753 <cw1753@nyu.edu>
2015-04-14 19:10:44 -0400

    Update checkclubevent.php

--- checkclubevent.php
+++ checkclubevent.php
@@ -3,8 +3,9 @@
 	session_start();
 	if (!(isset($_SESSION['login']) && $_SESSION['login'] != '')) {
 		header ("Location: login.php");
-	//get the pid
-	$personId= $_SESSION['userName']; 
+	}
+	//get user id
+	$userid= $_SESSION['userName']; 
 ?>
 
 <?php
@@ -18,7 +19,7 @@
 <?php
 //create query
 	$checkAdmin = $connection->prepare("SELECT clubid FROM role_in WHERE pid=? and role='Admin'");
-	$checkAdmin->bind_param("i", $personId);
+	$checkAdmin->bind_param("i", $userid);
 	$checkAdmin->execute();
 	$checkAdmin->bind_result($clubid);
 	if(!$checkAdmin){
