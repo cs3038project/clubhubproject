700fb85e130dfe6a57e0abfce7892484d7ca00b6
cw1753 <cw1753@nyu.edu>
2015-04-14 21:38:01 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,6 +2,6 @@
 session_start();
 if(session_destroy()) // Destroying All Sessions
 {
-header("Location: login2.php"); // Redirecting To Home Page
+header("Location: login.php"); // Redirecting To Home Page
 }
 ?>
