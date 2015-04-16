86c3073384ee824caf7cf83427b28e9c012e4b8d
cw1753 <cw1753@nyu.edu>
2015-04-16 13:35:14 -0400

    Update publicinfo.php

--- publicinfo.php
+++ publicinfo.php
@@ -45,10 +45,6 @@
 			<h1>  </h1>
 		</div>
 		
-   
-	
-
-	
 		<!-- Header -->
 			<header id="header" class="alt">
 				<h1><strong><a href="index.html"></a></strong> </h1>
@@ -58,7 +54,7 @@
 						<li><a href="homepage.php">Home</a></li>
 						<li><form action = "clubprofile.php" method = "GET"></li>
 						<table>
-					  <li><tr><td></td><td><input type= "text" id = "clubname"name = "clubname" placeholder="search for a club " required/></td></tr></li>
+					  <li><tr><td><input type= "text" id = "clubname"name = "clubname" placeholder="search for a club " required/></td></tr></li>
 	  				<li><tr><td><input type = "submit" id = "submit" name = "submit" value = "Find Club" ></td></tr></li>
 					</table>
 					</ul>
@@ -99,7 +95,7 @@
 			while($topicList->fetch()){
 		?>
 			<ul>
-			<li><a href="Display clubs.php?topic=<?php echo $topic;?>"><?php echo $topic; ?></a></li>
+			<li><a href="displayclubs.php?topic=<?php echo $topic;?>"><?php echo $topic; ?></a></li>
 			</ul>
 
 			
@@ -116,101 +112,7 @@
 					</div>
 				</section>
 
-			<!-- Two 
-				<section id="two" class="wrapper style2 special">
-					<div class="container">
-						<header class="major">
-							<h2>Fusce ultrices fringilla</h2>
-							<p>Club Topics</p>
-						</header>
-						<div class="row 150%">
-							<div class="6u 12u$(xsmall)">
-								<div class="image fit captioned">
-									<img src="images/pic02.jpg" alt="" />
-									<h3>Lorem ipsum dolor sit amet.</h3>
-									
-								</div>
-							</div>
-							<div class="6u$ 12u$(xsmall)">
-								<div class="image fit captioned">
-									<img src="images/pic03.jpg" alt="" />
-									<h3>Illum, maiores tempora cupid?</h3>
-								</div>
-							</div>
-						</div>
-						<ul class="actions">
-							<li><a href="#" class="button special big">Nulla luctus</a></li>
-							<li><a href="#" class="button big">Sed vulputate</a></li>
-						</ul>
-					</div>
-				</section> -->
-
-			<!-- Three 
-				<section id="three" class="wrapper style1">
-					<div class="container">
-						<header class="major special">
-							<h2>Mauris vulputate dolor</h2>
-							<p>Feugiat sed lorem ipsum magna</p>
-						</header>
-						<div class="feature-grid">
-							<div class="feature">
-								<div class="image rounded"><img src="images/pic04.jpg" alt="" /></div>
-								<div class="content">
-									<header>
-										<h4>Lorem ipsum</h4>
-										<p>Lorem ipsum dolor sit</p>
-									</header>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore esse tenetur accusantium porro omnis, unde mollitia totam sit nesciunt consectetur.</p>
-								</div>
-							</div>
-							<div class="feature">
-								<div class="image rounded"><img src="images/pic05.jpg" alt="" /></div>
-								<div class="content">
-									<header>
-										<h4>Recusandae nemo</h4>
-										<p>Ratione maiores a, commodi</p>
-									</header>
-									<p>Animi mollitia optio culpa expedita. Dolorem alias minima culpa repellat. Dolores, fuga maiores ut obcaecati blanditiis, at aperiam doloremque.</p>
-								</div>
-							</div>
-							<div class="feature">
-								<div class="image rounded"><img src="images/pic06.jpg" alt="" /></div>
-								<div class="content">
-									<header>
-										<h4>Laudantium fugit</h4>
-										<p>Possimus ex reprehenderit eaque</p>
-									</header>
-									<p>Maiores iusto inventore.</p>
-								</div>
-							</div>
-							<div class="feature">
-								<div class="image rounded"><img src="images/pic07.jpg" alt="" /></div>
-								<div class="content">
-									<header>
-										<h4>Porro aliquam</h4>
-										<p>Quaerat, excepturi eveniet laboriosam</p>
-									</header>
-									<p>Vitae earum unde, .</p>
-								</div>
-							</div>
-						</div>
-					</div>
-				</section>
-
-			!-- Four 
-				<section id="four" class="wrapper style3 special">
-					<div class="container">
-						<header class="major">
-							<h2>Aenean elementum ligula</h2>
-							<p>Feugiat sed lorem ipsum magna</p>
-						</header>
-						<ul class="actions">
-							<li><a href="#" class="button special big">Get in touch</a></li>
-						</ul>
-					</div>
-				</section> 
-
-		 Footer -->
+		 <!-- Footer -->
 			<footer id="footer">
 				<div class="container">
 					<ul class="icons">
