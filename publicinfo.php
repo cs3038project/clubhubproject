c83a9309caac798d936c7b0a7f56bb7dc1f6a100
Ceyron Hawthorne <loremIpsum1771@users.noreply.github.com>
2016-09-17 01:22:15 -0400

    Update publicinfo.php

--- publicinfo.php
+++ publicinfo.php
@@ -29,7 +29,6 @@
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
-		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
 		<script src="js/jquery.min.js"></script>
 		<script src="js/skel.min.js"></script>
 		<script src="js/skel-layers.min.js"></script>
